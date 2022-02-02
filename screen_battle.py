0b5a0263ac1622380258521930de15d2a9c9b5b6
ehnry055 <henry.g.choi@gmail.com>
2022-02-01 23:35:10 -0500

    finished battle lol

--- screen_battle.py
+++ screen_battle.py
@@ -22,22 +22,43 @@ class Screen_Battle(Frame):
         '''
         This method creates all of the (initial) widgets for the battle page.
         '''
-        Label(self, text = "You").grid(row = 4, column = 0, sticky = N)
-        Label(self, text = "Computer").grid(row = 4, column = 14, sticky = N)
+        
+        self.button = Button(self, text = "Attack", fg = "Red", command = self.attack_clicked)
+        self.button.grid(row = 0, column = 1, sticky = N)
 
-        you = PhotoImage(file="images/"+ str(self.player1.large_image))
-        image1 = Label(self, image = you, )
-        image1.photo = you
-        image1.grid(row = 5, column = 0, sticky = W)
+        Label(self, text = "You").grid(row = 3, column = 1, sticky = N)
+        Label(self, text = "Computer").grid(row = 3, column = 2, sticky = N)
 
-        cpu = PhotoImage(file="images/"+ str(self.player2.large_image))
-        image2 = Label(self, image = cpu, )
-        image2.photo = cpu
-        image2.grid(row = 5, column = 14, sticky = E)
+        for i in range(1, 3):
+            if i == 1:
+                p = self.player1
+            else:
+                p = self.player2
 
-        Label(self, text = f"{self.player1.hit_points} HP").grid(row = 6, column = 0, sticky = N)
-        Label(self, text = f"{self.player2.hit_points} HP").grid(row = 6, column = 14, sticky = N)
-            
+            character = PhotoImage(file="images/" + str(p.large_image))
+            image = Label(self, image = character, )
+            image.photo = character
+            image.grid(row = 4, column = i, sticky = W)
+
+        self.totalhp1 = self.player1.hit_points
+        self.totalhp2 = self.player2.hit_points
+
+        self.hp1 = Label(self, text = f"{self.player1.hit_points}/{self.totalhp1} HP")
+        self.hp1.grid(row = 5, column = 1, sticky = N)
+
+        self.hp2 = Label(self, text = f"{self.player2.hit_points}/{self.totalhp2} HP")
+        self.hp2.grid(row = 5, column = 2, sticky = N)
+
+
+
+        self.desc1 = Label(self, text= "")
+        self.desc1.grid(row= 0, column = 2, sticky = N)
+        
+        self.desc2 = Label(self, text= "")
+        self.desc2.grid(row= 1, column = 2, sticky = N)
+
+        self.winner = Label(self, text= "", fg = "Blue")
+        self.winner.grid(row= 2, column = 2, sticky = N)
             
         
     def attack_clicked(self):
@@ -52,9 +73,25 @@ class Screen_Battle(Frame):
             To remove a widget, use the destroy() method. For example:
     
                 self.button.destroy()   
-        '''        
-        pass
-                                            
+        '''
+        
+        self.desc1["text"] = f"{self.player1.attack(self.player2)}"
+        self.desc2["text"] = f"{self.player2.attack(self.player1)}"
+
+        self.hp1["text"] = f"{self.player1.hit_points}/{self.totalhp1} HP"
+        self.hp2["text"] = f"{self.player2.hit_points}/{self.totalhp2} HP"
+
+        if self.player1.hit_points <= 0 or self.player2.hit_points <= 0:
+            if self.player1.hit_points <= 0:
+                self.winner["text"] = f"{self.player1.name} is victorious!"
+            if self.player2.hit_points <= 0:
+                self.winner["text"] = f"{self.player2.name} is victorious!"
+        
+            self.button.destroy()
+            Button(self, text = "Exit!", fg = "Red", command = self.exit_clicked).grid(row = 0, column = 1, sticky = N)
+
+
+
     def exit_clicked(self):
         ''' This method is called when the Exit button is clicked. 
             It passes control back to the callback method. '''        
